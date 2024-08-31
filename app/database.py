0849b5ebe38ccbdaa3561095592f39f6902f9d03
marcoslima12 <marcosantonio-10@live.com>
2024-08-31 14:29:43 -0300

    update routers structure

--- app/database.py
+++ app/database.py
@@ -14,3 +14,10 @@ DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTG
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
