cba0b0aac4103259cdb40f0220026057c1d3a351
marcoslima12 <marcosantonio-10@live.com>
2024-08-31 11:02:42 -0300

    actions folder set

--- app/actions/task.py
+++ app/actions/task.py
@@ -1,22 +1,6 @@
 from sqlalchemy.orm import Session
-from app.models.user import User
 from app.models.task import Task
 from app.schemas.task import TaskCreate
-from app.schemas.user import UserCreate
-
-
-def get_user(db: Session, user_id: int):
-  return db.query(User).filter(User.id == user_id).first()
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-  return db.query(User).offset(skip).limit(limit).all()
-
-def create_user(db: Session, user: UserCreate):
-  db_user = User(email=user.email, fullname=user.fullname)
-  db.add(db_user)
-  db.commit()
-  db.refresh(db_user)
-  return db_user
 
 def create_task(db: Session, task: TaskCreate, user_id: int):
     db_task = Task(title=task.title, desc=task.desc, deadline=task.deadline, isDone=task.isDone, user_id=user_id)
